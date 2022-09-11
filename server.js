9607d9bbcafe4ea3dd4a2e1167d6b161d8fe211c
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-09-11 11:11:29 -0600

    making progress on inquirer prompts

--- server.js
+++ server.js
@@ -81,8 +81,11 @@ function viewAllRoles() {
 
 function viewAllEmployees() {
     connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id FROM employee, role, department WHERE role.id = employee.role_id AND department.id = role.department_id;',
-    // role.title AS role, role.salary as salary,
-    // INNER JOIN role ON employee.role_id = role.title
+    
+    //============================================//
+    //TODO: add link to manager name instead of id
+    //============================================//
+
     function(err, res) {
         if (err) throw err;
         console.table(res);
@@ -101,8 +104,7 @@ function addADepartment() {
         connection.query(`INSERT INTO department(name) VALUES ('${input.newDepartment}')`,
             function(err, res) {
                 if (err) throw err;
-                console.table(res);
-                viewAllDepartments();
+                console.log('New department added');
                 promptUser();
             }
         )
@@ -113,24 +115,71 @@ function addARole() {
     inquirer.prompt([
         {
             type: 'input',
-            message: 'Name the new role', 
+            message: 'Enter the title of the new role', 
             name: 'newRoleTitle'
         },
         {
             type: 'input',
-            message: 'What is this roles salary?',
+            message: 'Enter the salary for the new role',
             name: 'newRoleSalary'
         },
         {
-            type: ''
+            type: 'input',
+            message: 'Input the department id for this role',
+            name: 'newRoleDepartment'
         }
-
+        //==========================================//
+        //TODO: switch input from id to just enter the department name
+        //=========================================//
+        
+        // {
+        //     type: 'list',
+        //     message: 'Which department does the new role belong to?',
+        //     name: 'newRoleDepartment',
+        //     choices: ['Sales', 'Marketing', 'Accounting', 'RnD', 'Legal', 'Executive', 'Customer Service']
+        // }
     ]).then(function (input) {
-        connection.query(`INSERT INTO role(title, salary) VALUES ('${input.newRoleTitle}', ${input.newRoleSalary})`,
+        connection.query(`INSERT INTO role(title, salary, department_id) VALUES ('${input.newRoleTitle}', ${input.newRoleSalary}, '${input.newRoleDepartment}')`,
             function(err, res) {
                 if (err) throw err;
-                console.table(res);
-                viewAllRoles();
+                console.log('New role added');
+                promptUser();
+            }
+        )
+    })
+}
+
+//================================================//
+//Revisit to ask for role and manager, rather than ids
+//================================================//
+
+function addAnEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the employees first name?', 
+            name: 'newEmployeeFirstName'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees last name?',
+            name: 'newEmployeeLastName'
+        },
+        {
+            type: 'input',
+            message: 'What is the role id?',
+            name: 'newEmployeeRole'
+        },
+        {
+            type: 'input',
+            message: 'What is the managers id?',
+            name: 'newEmployeeManager'
+        }
+    ]).then(function (input) {
+        connection.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('${input.newEmployeeFirstName}', '${input.newEmployeeLastName}', ${input.newEmployeeRole}, ${input.newEmployeeManager})`,
+            function(err, res) {
+                if (err) throw err;
+                console.log('New employee added');
                 promptUser();
             }
         )
